a30a0e399760bf0293744f7f2295489b6b5a7ca4
davidhrgl <davidhrgl@gmail.com>
2019-06-03 22:29:54 -0500

    modificando el index

--- index.php
+++ index.php
@@ -1,1 +1,181 @@
-index.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Control de Camiones de Recolecciión</title>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="assets/css/bootstrap.css">
+<link rel="stylesheet" href="assets/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="assets/css/alertify.min.css">
+<link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="table-wrapper">
+            <div class="table-title">
+                <div class="row">
+                    <div class="col-sm-6">
+						<h2>Control de <b>Camiones de Recoleccón</b></h2>
+					</div>
+					<div class="col-sm-6">
+						<button type="button" class="btn btn-success" data-toggle="modal" data-target="#addCamionModal"><i class="material-icons">&#xE147;</i>Agregar Camion</button>
+					</div>
+                </div>
+            </div>
+			<div class='clearfix'></div>
+			<hr>
+			<div id="loader"></div><!-- Carga de datos ajax aqui -->
+			<div id="resultados"></div><!-- Carga de datos ajax aqui -->
+			<div class='outer_div'></div><!-- Carga de datos ajax aqui -->
+			<div class="table-responsive">
+				<table id="datatable-buttons" class="table">
+					<thead>
+						<tr>
+							<th>Id Vehiculo</th>
+							<th>Tipo Camion</th>
+							<th>SubMarca</th>
+							<th>Modelo</th>
+							<th>Kilometraje</th>
+							<th>Placas</th>
+							<th>Economico</th>
+							<th>Numero de contenedores</th>
+							<th>Tipo de Contenedores</th>
+							<th>Capacidad (Ton)</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody id="tbody_user"></tbody>
+				</table>
+			</div>
+        </div>
+	</div>
+
+	<!--Ventana Modal, Agregar Camion-->
+	<!-- Large modal -->
+	<!-- Modal -->
+	<div id="addCamionModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="addCamionModal" aria-hidden="true">
+		<div class="modal-lg modal-dialog">
+			<div class="modal-content">
+				<form name="add_camion" id="add_camion">
+					<div class="modal-header">
+						<h4 class="modal-title">Agregar Camión</h4>
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					</div>
+					<div class="modal-body">
+						<div class="form-group">
+							<label>Tipo de Vehículo</label>
+							<input type="text" name="Tipo" id="tipo" class="form-control" required>
+	
+						</div>
+						<div class="form-group">
+							<label>Marca</label>
+							<input type="text" name="Marca" id="marca" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Submarca</label>
+							<input type="text" name="Submarca" id="submarca" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Modelo</label>
+							<input type="text" name="Modelo" id="modelo" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Kilometraje</label>
+							<input type="number" name="Kilometraje" id="kilome" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Placa</label>
+							<input type="text" name="Placa" id="placa" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Número Económico</label>
+							<input type="number" name="Número Económico" id="num_econom" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Número de Póliza</label>
+							<input type="text" name="Número de Póliza" id="num_poli" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Número de Contenedores</label>
+							<input type="number" name="Número de Contenedores" id="num_cont" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Tipo de Contenedores</label><br>
+							<div class="form-check">
+								<input class="form-check-input" type="radio" name="contenedor" id="contenedor1" value="Desechos Organicos" checked>
+								<label class="form-check-label" for="contenedor1">
+									Desechos Orgánicos
+								</label>
+							</div>
+							<div class="form-check">
+								<input class="form-check-input" type="radio" name="contenedor" id="contenedor2" value="Desechos Inorganicos">
+								<label class="form-check-label" for="contenedor2">
+									Desechos Inorgánicos
+								</label>
+							</div>
+							<div class="form-check ">
+								<input class="form-check-input" type="radio" name="contenedor" id="contenedor3" value="Valorizables y Reutizables">
+								<label class="form-check-label" for="contenedor3">
+									Desechos Valorizables y Reutilizables
+								</label>
+							</div>
+						</div>
+						<div class="form-group">
+							<label>Capacidad en Toneladas (c/u)</label>
+							<input type="text" name="Capacidad" id="capacidad" class="form-control" required>
+						</div>
+						<div class="form-group">
+							<label>Descripción</label>
+							<textarea name="Descripción" id="descripcion" cols="70" rows="9" class="form-control" required></textarea>
+						</div>
+						<div class="form-group">
+							<label>Status</label><br>
+							<div class="form-check">
+								<input class="form-check-input" type="radio" name="status_vehicle" id="status_vehicle1" value="Disponible" checked>
+								<label class="form-check-label" for="status_vehicle1">
+									Disponible
+								</label>
+							</div>
+							<div class="form-check">
+								<input class="form-check-input" type="radio" name="status_vehicle" id="status_vehicle2" value="En Reparacion">
+								<label class="form-check-label" for="status_vehicle2">
+									En Reparación
+								</label>
+							</div>
+							<div class="form-check ">
+								<input class="form-check-input" type="radio" name="status_vehicle" id="status_vehicle3" value="Fuera de Servicio">
+								<label class="form-check-label" for="status_vehicle3">
+									Fuera de Servicio
+								</label>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Cancelar" id="close_modal">
+						<input type="button" class="btn btn-success" value="Guardar datos" id = "btnaddCamion">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+	<script src="assets/js/jquery-3.3.1.js"></script>
+	<script src="assets/js/jquery.dataTables.min.js"></script>
+	<script src="assets/js/dataTables.bootstrap4.min.js"></script>
+	<script src="assets/js/alertify.min.js"></script>
+	<script src="assets/js/api.js"></script>
+	<script src="assets/js/modal.js"></script>
+	<script src="assets/js/util.js"></script>
+	<script>
+	$(document).ready(function () {
+		getdata();
+	});
+	</script>
+</body>
+</html>                                		                            
